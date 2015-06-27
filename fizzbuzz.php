327e2394538688603658677e593a859f2015502b
Andrew Huggins <andrewhuggins@gmail.com>
2015-06-27 19:13:24 -0400

    Update fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -9,6 +9,7 @@ class FizzBuzz
 	protected $firstDenominator;
 	protected $secondDenominator;
 	protected $beginRange;
+	protected $print;
 	
 	function __construct($maxRange = 50, $firstDenominator = 3, $secondDenominator = 5, $beginRange = 1)
 	{
@@ -19,21 +20,41 @@ class FizzBuzz
 		$this->FizzBuzz();
 	}
 
+	/**
+	 * Create a loop based on the parameters passed in constructor, loop through the values and check
+	 * if divisible by 3 or 5 (or passed denominators). Then print the value.
+	 */
 	public function FizzBuzz()
 	{
 		for ( $this->i = $this->beginRange; $this->i <= $this->maxRange; $this->i++ ) { 
-			if ( $this->divisible( $this->firstDenominator ) && $this->divisible( $this->secondDenominator ) ) {
-				print 'FizzBuzz' . PHP_EOL;
-			} elseif ( $this->divisible( $this->firstDenominator ) ) {
-				print 'Fizz' . PHP_EOL;
-			} elseif ( $this->divisible( $this->secondDenominator ) ) {
-				print 'Buzz' . PHP_EOL;
-			} else {
-				print $this->i . PHP_EOL;
+			$this->print = $this->i;
+			if ($this->divisible( $this->firstDenominator )) {
+				$this->print = 'Fizz';
 			}
+			if ($this->divisible( $this->secondDenominator )) {
+				$this->print = $this->both();
+			}
+			print $this->print . PHP_EOL;
 		}
 	}
 
+	/**
+	 * Check if the integer in the loop is divisible by the firstDenominator 
+	 * @return string  Either FizzBuzz or Fizz.
+	 */
+	protected function both()
+	{
+		if ($this->print == 'Fizz') {
+			return $this->print . 'Buzz';
+		}
+		return 'Buzz';
+	}
+
+	/**
+	 * Check to see if the $i is divisible by the passed $m (modulus)
+	 * @param  int $m 
+	 * @return boolean    Whether or not the number is evenly divisble
+	 */
 	protected function divisible($m)
 	{
 		if (($this->i % $m) == 0) {
